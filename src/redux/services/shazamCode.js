f027ec30462fb0843696937818767d9486e62e10
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-06-09 17:36:56 +0300

    feat/created api

---
+++ src/redux/services/shazamCode.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core7.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-KEY', '87a213de5emshb7203bccc9a90bap12bbc1jsn188405f12602');
+      headers.set('X-RapidAPI-Host', 'shazam-core7.p.rapidapi.com');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in-world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
