b961ba2ea4718f9505f1e36e049150e72e2502a6
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-06-11 17:40:37 +0300

    feat/changed discover comp

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCode';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
@@ -15,15 +19,28 @@ const Discover = () => {
     <div className="flex flex-col ">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
-        <select onChange={() => {}} value="" className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
           {genres.map((genre) => (
-            <option value={genre.value} key={genre.value}>{genre.title}</option>
+            <option value={genre.value} key={genre.value}>
+              {genre.title}
+            </option>
           ))}
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, index) => (
-          <SongCard key={song.key} song={song} index={index} />
+          <SongCard
+            key={song.key}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            data={data}
+            index={index}
+          />
         ))}
       </div>
     </div>
